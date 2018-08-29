7cf19e6efaf718ae0a73793965eb38c57d67bd9a
Krishna Teja <packetcode@gmail.com>
2018-08-29 20:20:48 +0000

    paytm

--- resources/views/appl/product/pages/checkout_success.blade.php
+++ resources/views/appl/product/pages/checkout_success.blade.php
@@ -9,6 +9,9 @@
 <h1 class="text-success"><i class="fa fa-check-circle"></i> Success</h1>
 <hr>
 
+@if(request()->get('MID'))
+	{{ dd(request()->all())}}
+@endif
 <p> Your transaction with reference number XXXX was successful. The login details will be mailed to you shortly.
 <br>In case of any query contact the adminstrator, the contact details are mentioned in this <a href="{{ route('contact-corporate')}}">link</a></p>
 
