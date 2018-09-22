0a1920ed7336dfed61e22ab776e448aa62dd1e3f
Krishna Teja <packetcode@gmail.com>
2018-09-22 13:21:57 +0000

    domain changes

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -34,10 +34,15 @@ class ProductController extends Controller
             $parsed = parse_url($url);
             $exploded = explode('.', $parsed["host"]);
             $subdomain = $exploded[0];
-            //$file = "../json/".$subdomain.".json";
+            $file = "../json/".$subdomain.".json";
             
             $client = Client::where('slug',$subdomain)->first();
-            
+            if(!file_exists($file)){
+                $newJsonString = json_encode($client, JSON_PRETTY_PRINT);
+                file_put_contents(base_path('json/'.$client->slug.'.json'), stripslashes($newJsonString));
+            }
+
+
 
             if($client){
                 $filedata = $client;//json_decode(file_get_contents($file));
