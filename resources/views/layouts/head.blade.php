a297e0c2602621d3dd4225c4978280bbb08c7136
Krishna Teja <shaadomanthra@yahoo.com>
2018-12-06 16:33:34 +0000

    paytm changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,6 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="PacketPrep is an online video learning preparation platform for quantitative aptitude, logical reasoning, mental ability, general english and interview skills.">
+      <meta name="keywords" content="quantitative aptitude, mental ability, learning, simple, interresting, logical reasoning, general english, interview skills, bankpo, sbi po, ibps po, sbi clerk, ibps clerk, government job preparation, bank job preparation, campus recruitment training, crt, online lectures, gate preparation, gate lectures">
+      <meta name="author" content="Krishna Teja G S">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Packet Prep') }}</title>
