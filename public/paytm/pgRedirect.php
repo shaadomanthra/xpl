7632932f61463769169845bcf652f8b4ae241b62
Krishna Teja <shaadomanthra@yahoo.com>
2018-12-10 06:23:50 +0000

    paytm changes

--- public/paytm/pgRedirect.php
+++ public/paytm/pgRedirect.php
@@ -23,6 +23,7 @@ $paramList["INDUSTRY_TYPE_ID"] = $INDUSTRY_TYPE_ID;
 $paramList["CHANNEL_ID"] = $CHANNEL_ID;
 $paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 $paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
+$paramList["CALLBACK_URL"] = "https://packetprep.com/paytm/pgResponse.php";
 
 /*
 $paramList["CALLBACK_URL"] = "http://localhost/PaytmKit/pgResponse.php";
