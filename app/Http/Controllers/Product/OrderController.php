0fb1cbbc9fb80aeb2e13eefe1cb2c63b0ec3e375
Krishna Teja <shaadomanthra@yahoo.com>
2018-12-10 06:05:08 +0000

    paytm changes

--- app/Http/Controllers/Product/OrderController.php
+++ app/Http/Controllers/Product/OrderController.php
@@ -135,7 +135,7 @@ class OrderController extends Controller
             $order->txn_id = $_POST['TXNID'];
             if ($_POST["STATUS"] == "TXN_SUCCESS"){
               $order->status = 1;
-              $valid_till = $valid_till = date('Y-m-d H:i:s', strtotime(date("Y-m-d H:i:s") .' + '.(24)*31).' days'));
+              $valid_till = date('Y-m-d H:i:s', strtotime(date("Y-m-d H:i:s") .' + '.(24*31).' days'));
               if(!$user->products->contains($product->id))
               $user->products()->attach($order->product_id,['validity'=>24,'created_at'=>date("Y-m-d H:i:s"),'valid_till'=>$valid_till,'status'=>1]);
             }
