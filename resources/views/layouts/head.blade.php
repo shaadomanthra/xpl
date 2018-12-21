63f3a20dd26a45922d64c5ac48b479e6fe00cdc7
Krishna Teja <shaadomanthra@yahoo.com>
2018-12-21 12:06:47 +0000

    google meta tag

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,6 +9,7 @@
     <meta name="description" content="PacketPrep is an online video learning preparation platform for quantitative aptitude, logical reasoning, mental ability, general english and interview skills.">
       <meta name="keywords" content="quantitative aptitude, mental ability, learning, simple, interresting, logical reasoning, general english, interview skills, bankpo, sbi po, ibps po, sbi clerk, ibps clerk, government job preparation, bank job preparation, campus recruitment training, crt, online lectures, gate preparation, gate lectures">
       <meta name="author" content="Krishna Teja G S">
+      <meta name="google-site-verification" content="yLlPK2iIdUaVEKFdZ5k9VV6IPC-a3GoCK_aBGmYt-xE" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
