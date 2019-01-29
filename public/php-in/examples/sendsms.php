1854fc2941e9392de4804033b2cd97c54eb6eb72
Krishna Teja <shaadomanthra@yahoo.com>
2019-01-29 12:08:41 +0000

    some changes

--- public/php-in/examples/sendsms.php
+++ public/php-in/examples/sendsms.php
@@ -5,7 +5,7 @@ require('../textlocal.class.php');
 $textlocal = new Textlocal('packetcode@gmail.com', 'c1120d3477ff90880eb3327e1526a4f76114d87812ad7d9da247eac6fdb74f13');
 
 $numbers = array(918688079590,919515125110);
-$sender = 'PCKPRP';
+$sender = 'PKTPRP';
 $message = 'Thank you for registering with PacketPrep. Your Verification Code is 45678';
 
 try {
