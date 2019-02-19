fbfab9dad645542d43741e711c85d26f766770a9
Krishna Teja <shaadomanthra@yahoo.com>
2019-02-19 10:09:20 +0000

    some changes

--- resources/views/appl/product/snippets/adminmenu.blade.php
+++ resources/views/appl/product/snippets/adminmenu.blade.php
@@ -7,13 +7,14 @@
 	@if(\Auth::user()->checkRole(['administrator']))
 	<a href="{{ route('product.index')}}" class="list-group-item list-group-item-action {{ request()->is('product*') ? 'active' : ''}}"><i class="fa fa-inbox"></i> Products</a>
 	<a href="{{ route('job.index')}}" class="list-group-item list-group-item-action {{ request()->is('job*') ? 'active' : ''}}"><i class="fa fa-list"></i> Jobs</a>
-	<a href="{{ route('form.index')}}" class="list-group-item list-group-item-action {{ request()->is('form*') ? 'active' : ''}}"><i class="fa fa-list"></i> Applicants</a>
 	<a href="{{ route('order.list')}}" class="list-group-item list-group-item-action {{ request()->is('admin/transactions*') ? 'active' : ''}}"><i class="fa fa-list"></i> Transactions</a>
 	<a href="{{ route('material')}}" class="list-group-item list-group-item-action {{ request()->is('material') ? 'active' : ''}}"><i class="fa fa-telegram"></i> Material</a>
 	<a href="{{ route('exam.index')}}" class="list-group-item list-group-item-action {{ request()->is('exam') ? 'active' : ''}}"><i class="fa fa-th"></i> Exams</a>
 	<a href="{{ route('coupon.index')}}" class="list-group-item list-group-item-action {{ request()->is('coupon') ? 'active' : ''}}"><i class="fa fa-bars"></i> Coupons</a>
 	@endif
 	<a href="{{ route('ambassador.list2')}}" class="list-group-item list-group-item-action {{ request()->is('admin/ambassador*') ? 'active' : ''}}"><i class="fa fa-bars"></i> Ambassadors</a>
+	
+	<a href="{{ route('form.index')}}" class="list-group-item list-group-item-action {{ request()->is('form*') ? 'active' : ''}}"><i class="fa fa-list"></i> Applicants</a>
 	<a href="{{ route('college.index')}}" class="list-group-item list-group-item-action {{ request()->is('college') ? 'active' : ''}}"><i class="fa fa-bars"></i> Colleges</a>
 	<a href="{{ route('branch.index')}}" class="list-group-item list-group-item-action {{ request()->is('branch') ? 'active' : ''}}"><i class="fa fa-bars"></i> Branch</a>
 	<a href="{{ route('zone.index')}}" class="list-group-item list-group-item-action {{ request()->is('zone') ? 'active' : ''}}"><i class="fa fa-bars"></i> Zone</a>
