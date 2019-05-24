4c46bf5ec4938d5f1eadc2dea85a9069c4f837e4
Krishna Teja <shaadomanthra@yahoo.com>
2019-05-24 09:49:12 +0000

    some changes

--- resources/views/appl/product/customer/bootcamp.blade.php
+++ resources/views/appl/product/customer/bootcamp.blade.php
@@ -20,14 +20,18 @@
        <ul>
         <li>May 20th to May 31st  2019
           <ul>
-           <li>Batch 1 - 9:00 am to 1:00 pm (Closed)</li>
-           <li>Batch 2 - 2:00 pm to 6:00 pm (Closed)</li> 
+           <li>Batch 1 - 9:00 am to 1:00 pm (Closed) </li>
           </ul>
         </li> 
-       <li>June 3rd to June 14th  2019
+       <li>June 6th to June 17th  2019
           <ul>
-           <li>Batch 3 - 9:00 am to 1:00 pm (Closed)</li>
-           <li>Batch 4 - 2:00 am to 6:00 pm (Closed)</li>
+           <li>Batch 2 - 9:00 am to 1:00 pm (Closed)</li>
+           <li>Batch 3 - 2:00 am to 6:00 pm (Closed)</li>
+          </ul>
+        </li> 
+        <li>June 18th to June 29th  2019
+          <ul>
+           <li>Batch 4 - 9:00 am to 1:00 pm <span class="badge badge-success">Open</span></li>
           </ul>
         </li> 
         </ul>  
@@ -41,7 +45,7 @@
         <h2 > <i class="fa fa-rupee"></i>  6,000 </h2>
         
       </div>
-      <!--
+      
       <div class="bg-light p-3 border rounded mb-3">
         <h3> Reserve your seat now</h3>
       <a href="{{ url('productpage/cb') }}">
@@ -51,7 +55,7 @@
       <button class="btn btn-lg btn-primary mb-3"> Part Payment</button>
       </a>
     </div>
-  -->
+
 
       <div class="p-3 bg-warning mb-3 rounded"><b>Note:</b><br> Students who need 4 or 5 weeks internship certificate can do a mini project with us FREE of cost (only for bootcamp participants) <a href="{{ route('bootcamp')}}#mini-project">more details here</a></div>
 
