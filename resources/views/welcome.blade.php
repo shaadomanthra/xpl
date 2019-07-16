ecbca25d5c536ecf4195bd58c6da2d393f5a6b8d
Krishna Teja <shaadomanthra@yahoo.com>
2019-07-16 08:55:16 +0000

    some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 	<div class="pt-5 p-3 p-md-0 pt-md-5" >
     <h2 class="heading3 text-white " >Best platform to prepare for</h2>
     <h1 class="heading1 text-white mt-2 mb-1" >Campus Placements</h1>
-    <h4 class="heading4 text-white mb-5" >Quantitative Aptitude, Reasoning, Verbal Ability, Programming Concepts and Coding</h4>
+    <h4 class="heading4 text-white mb-5" style="line-height: 30px;" >Quantitative Aptitude, Reasoning, Verbal Ability, Programming Concepts and Coding</h4>
     <a href="{{ route('register.type')}}"><button class="btn btn-outline-light btn-lg">Register Now</button></a>
 	</div>
 </div>
