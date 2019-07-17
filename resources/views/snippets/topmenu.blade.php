e22a55c8e85675fed5e259c9d56bb3773e457e71
Krishna Teja <shaadomanthra@yahoo.com>
2019-07-17 16:30:42 +0000

    come changes

--- resources/views/snippets/topmenu.blade.php
+++ resources/views/snippets/topmenu.blade.php
@@ -1,11 +1,11 @@
 
  <nav class="navbar navbar-expand-lg navbar-dark " >
             @guest
-            <a class="navbar-brand abs" href="{{ url('/') }}">
+            <a class="navbar-brand abs" href="{{ url('/') }}" aria-label="Homepage">
                 <img src="{{ asset('img/packetprep-logo-small.png') }}" width="60px" class="logo-main ml-md-1" />
             </a>
             @else
-            <a class="navbar-brand abs" href="{{ url('/dashboard') }}">
+            <a class="navbar-brand abs" href="{{ url('/dashboard') }}" aria-label="Dashboard">
                 <img src="{{ asset('img/packetprep-logo-small.png') }}" width="60px" class="logo-main ml-md-1" />
             </a>   
             @endguest
@@ -21,31 +21,31 @@
                     @else
 
                     
-                    <li class="mr-3"><a class="nav-link" href="{{ route('dashboard') }}"
+                    <li class="mr-3"><a class="nav-link" href="{{ route('dashboard') }}" aria-label="Dashboard page"
                             ><i class="fa fa-dashboard"></i>
                            Dashboard
                              </a></li>
                     
                     @endguest
-                    <li class="mr-3 "><a class="nav-link " href="{{ url('course') }}"><i class="fa fas fa-youtube-play"></i> Courses</a></li>
+                    <li class="mr-3 "><a class="nav-link " href="{{ url('course') }}" aria-label="PacketPrep Courses"><i class="fa fas fa-youtube-play"></i> Courses</a></li>
 
-                    <li class="mr-3 "><a class="nav-link " href="{{ url('firstacademy') }}"><i class="fa fas fa-gg"></i> Abroad Studies</a></li>
+                    <li class="mr-3 "><a class="nav-link " href="{{ url('firstacademy') }}" aria-label="Abroad Study Preparation"><i class="fa fas fa-gg"></i> Abroad Studies</a></li>
                     <!--
                     <li class="mr-3 "><a class="nav-link " href="{{ url('tracks') }}"><i class="fa fa fa-spotify"></i> Tracks</a></li>   -->
-                    <li class="mr-3 "><a class="nav-link " href="{{ url('targettcs') }}"><i class="fa fa-ravelry"></i> Target TCS</a></li>
+                    <li class="mr-3 "><a class="nav-link " href="{{ url('targettcs') }}" aria-label="Target TCS program"><i class="fa fa-ravelry"></i> Target TCS</a></li>
 
-                    <li class="mr-3 "><a class="nav-link " href="{{ url('ambassador') }}"><span class="badge badge-warning p-1 pl-2 pr-2"> Internship</span></a></li>
+                    <li class="mr-3 "><a class="nav-link " href="{{ url('ambassador') }}" aria-label="Internship at packetprep"><span class="badge badge-warning p-1 pl-2 pr-2"> Internship</span></a></li>
                     
                     <li class="nav-item dropdown mr-2">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="dropdown menu">
                            <i class="fa fa-bullseye"></i>  Programs <span class="caret"></span>
                        </a>
                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item"  href="{{ url('gigacode')}}"
+                        <a class="dropdown-item"  href="{{ url('gigacode')}}" aria-label="Gigacode program"
                             >
                             GigaCode 
                              </a>
-                            <a class="dropdown-item"  href="{{ url('bootcamp')}}"
+                            <a class="dropdown-item"  href="{{ url('bootcamp')}}" aria-label="Coding Bootcamp program"
                             >
                            Coding Bootcamp
                              </a>
@@ -57,8 +57,8 @@
                    
                     <!-- Authentication Links -->
                     @guest
-                    <li class="mr-2"><a class="nav-link " href="{{ route('login') }}"><i class="fa fa-sign-in"></i> Login</a></li>
-                    <li class="mr-2"><a class="nav-link " href="{{ route('register.type') }}"><i class="fa fa-user-plus"></i> Register</a></li>
+                    <li class="mr-2"><a class="nav-link " href="{{ route('login') }}" aria-label="Login page"><i class="fa fa-sign-in"></i> Login</a></li>
+                    <li class="mr-2"><a class="nav-link " href="{{ route('register.type') }}" aria-label="Registration page"><i class="fa fa-user-plus"></i> Register</a></li>
                     
                     @else
                    
