8f386fde5c25eddb14e32bdc56e88f641c233b0b
Krishna Teja <shaadomanthra@yahoo.com>
2019-08-07 01:53:31 +0000

    some changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -42,6 +42,13 @@
     @if(isset($jqueryui))
     <link href="{{ asset('jquery-ui/jquery-ui.min.css') }}" rel="stylesheet">
     @endif
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-9550642194256443",
+    enable_page_level_ads: true
+  });
+</script>
 </head>
     <body>
     <div id="app" >
