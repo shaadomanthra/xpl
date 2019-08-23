c19a03453be5cc194b2946ce1765193568834bd1
Krishna Teja <shaadomanthra@yahoo.com>
2019-08-23 14:25:21 +0000

    some changes

--- resources/views/appl/content/company/googleads.blade.php
+++ resources/views/appl/content/company/googleads.blade.php
@@ -1,8 +1,9 @@
-
-
 <ins class="adsbygoogle"
      style="display:block; text-align:center;"
      data-ad-layout="in-article"
      data-ad-format="fluid"
      data-ad-client="ca-pub-9550642194256443"
      data-ad-slot="5416437506"></ins>
+<script type="application/javascript">
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
