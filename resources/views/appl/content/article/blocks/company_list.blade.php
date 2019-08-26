a85cba631010771f2a66c7ec811d905ee73fd9ac
Krishna Teja <shaadomanthra@yahoo.com>
2019-08-26 07:16:54 +0000

    some changes

---
+++ resources/views/appl/content/article/blocks/company_list.blade.php
@@ -0,0 +1,45 @@
+<div class="col-12 col-md-6 col-lg-4">
+  <div class="card mb-4" >
+    <div class="card-body">
+      @if(file_exists('img/companies/wipro.jpg'))
+      <img srcset="{{ asset('img/companies/wipro.jpg') }} 320w"
+      sizes="(max-width: 320px) 280px"
+      src="{{ asset('img/companies/wipro.jpg') }} " class=" pt-3 pb-3 pr-4" alt="Wipro Recruitment Process" height="120px">
+      @endif
+      <h4 class="card-title article">Wipro placement papers and recruitment process</h4>
+      @if(file_exists('img/companies/wipro.jpg'))
+      <small><a href="wipro-placement-papers-recruitment-process" class="card-link"> readmore <i class="fa fa-angle-right"></i></a></small>
+      @endif
+    </div>
+  </div>
+</div>
+<div class="col-12 col-md-6 col-lg-4">
+  <div class="card mb-4" >
+    <div class="card-body">
+      @if(file_exists('img/companies/tcs.jpg'))
+      <img srcset="{{ asset('img/companies/tcs.jpg') }} 320w"
+      sizes="(max-width: 320px) 280px"
+      src="{{ asset('img/companies/tcs.jpg') }} " class=" pt-3 pb-3 pr-4" alt="Wipro Recruitment Process" height="120px">
+      @endif
+      <h4 class="card-title article">TCS NQT Ninja Digital Recruitment</h4>
+      @if(file_exists('img/companies/tcs.jpg'))
+      <small><a href="tcs-nqt-ninja-digital-recruitment" class="card-link"> readmore <i class="fa fa-angle-right"></i></a></small>
+      @endif
+    </div>
+  </div>
+</div>
+<div class="col-12 col-md-6 col-lg-4">
+  <div class="card mb-4" >
+    <div class="card-body">
+      @if(file_exists('img/companies/infosys.jpg'))
+      <img srcset="{{ asset('img/companies/infosys.jpg') }} 320w"
+      sizes="(max-width: 320px) 280px"
+      src="{{ asset('img/companies/infosys.jpg') }} " class=" pt-3 pb-3 pr-4" alt="Wipro Recruitment Process" height="120px">
+      @endif
+      <h4 class="card-title article">Infosys placement papers and interview questions</h4>
+      @if(file_exists('img/companies/tcs.jpg'))
+      <small><a href="infosys-placement-papers-interview-questions" class="card-link"> readmore <i class="fa fa-angle-right"></i></a></small>
+      @endif
+    </div>
+  </div>
+</div>
--- resources/views/appl/content/article/companies.blade.php
+++ resources/views/appl/content/article/companies.blade.php
@@ -1,56 +1,21 @@
 @extends('layouts.app2')
-@section('title', 'Companies Placement Preparation | PacketPrep')
+@section('title', 'Placement material for all companies | PacketPrep')
 @section('description', 'Here you will find placement preperation material for wipro,infosys, capgemini, delloite etc')
 @section('keywords', 'company placement preparation')
 @section('content')
 
-
 @include('flash::message')
 <div class="container">
-  <div class="row">
-
-    <div class="col-md-12">
-
-
       <div class="p-3 p-md-4 p-lg-5 bg-white company">
-
-        <h1 class="mb-5"> Company Placement Material
+        <h1 class="mb-2"> Placement material for all companies
         </h1>
-
+        <p>Preparing for placements is a herculean task. Searching for previous papers, marking important questions, learning the quant fundamentals,  then verbal, reasoning and even coding...ufff..its no easy task.  We went through this cycle, and everybody in the final year of graduation also does the same. So we thought it could be a good idea to collect everything relevant to placements and stack them in one place.  </p>
+        <p class="mb-5">Here you will find company-wise placement papers, practice questions for quantitative aptitude, verbal ability, reasoning, coding and interview questions. Do share this with your friends and post your feedback in the comments section. </p>
         
         <div class="row">
-        @foreach($companies as $company)
-          <div class="col-12 col-md-4">
-            <div class="card mb-4" >
-              <div class="card-body">
-                @if(file_exists('img/companies/'.$company.'.jpg'))
-                <img srcset="{{ asset('img/companies/'.$company.'.jpg') }} 320w"
-                  sizes="(max-width: 320px) 280px"
-                  src="{{ asset('img/companies/'.$company.'.jpg') }} " class="w-75 pt-3 pb-3 pr-4" alt="{{$company}} Recruitment Process">
-                @endif
-                <h4 class="card-title article">{{ucfirst($company)}} placement papers and recruitment process</h4>
-                @if(file_exists('img/companies/'.$company.'.jpg'))
-                <small><a href="{{ $company}}-placement-papers-recruitment-process" class="card-link"> readmore <i class="fa fa-angle-right"></i></a></small>
-                @endif
-              </div>
-            </div>
-          </div>
-        @endforeach
+          @include('appl.content.article.blocks.company_list')
         </div>
-
-        
-
       </div>
     </div>
-
-  </div>
-
-  
-
-
-</div> 
-
 </div>
-
-
 @endsection
