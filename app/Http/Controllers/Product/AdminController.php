d6593f14b94bb41ed3b96acb969da0ef3baddb34
Krishna Teja <shaadomanthra@yahoo.com>
2019-09-20 12:12:54 +0000

    some changes

--- app/Http/Controllers/Product/AdminController.php
+++ app/Http/Controllers/Product/AdminController.php
@@ -305,7 +305,7 @@ class AdminController extends Controller
         if($recent){
         $users = $user->where(function ($query) use ($item) {
                                 $query->where('name','LIKE',"%{$item}%")
-                                      ->orWhere('email', 'LIKE', "%{$item}%");
+                                      ->orWhere('email', 'LIKE', "%{$item}%")->orWhere('phone', 'LIKE', "%{$item}%");
                             })->orderBy('updated_at','desc')->paginate(config('global.no_of_records'));
         }elseif($metric){
             $m = Metric::where('name',$metric)->first();
@@ -314,7 +314,7 @@ class AdminController extends Controller
         }else
         $users = $user->where(function ($query) use ($item) {
                                 $query->where('name','LIKE',"%{$item}%")
-                                      ->orWhere('email', 'LIKE', "%{$item}%");
+                                      ->orWhere('email', 'LIKE', "%{$item}%")->orWhere('phone', 'LIKE', "%{$item}%");
                                       
                             })->orderBy('created_at','desc')->paginate(config('global.no_of_records'));
         
