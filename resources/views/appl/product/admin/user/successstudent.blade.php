d35bb84ce8bb4985a4f5e0cfed5f65d334568b46
vagrant <vagrant@homestead>
2019-11-07 11:42:24 +0000

    some changes

--- resources/views/appl/product/admin/user/successstudent.blade.php
+++ resources/views/appl/product/admin/user/successstudent.blade.php
@@ -9,7 +9,7 @@
 <h1 class="text-primary"><i class="fa fa-check-circle"></i> Registration Successful !</h1>
 <hr>
 
-<p>  You will recieve your login details through email. If you dont recieve login details on email then kindly check SPAM FOLDER, also you can use the following link to recieve the password on sms <a href="https://packetprep.com/user/password/forgot"> https://packetprep.com/user/password/forgot</a> <br><hr>In case of any error, kindly contact the adminstrator, the contact details are mentioned in this <a href="{{ route('contact-corporate')}}">link</a></p>
+<p>  You will recieve your login details through email. If you dont recieve login details on email then kindly check SPAM FOLDER, also you can use the following link to recieve the password on sms <a href="https://xplore.co.in/user/password/forgot"> https://xplore.co.in/user/password/forgot</a> <br><hr>In case of any error, kindly contact the adminstrator, the contact details are mentioned in this <a href="{{ route('contact-corporate')}}">link</a></p>
 
 <a href="{{ route('login')}}">
 	<button class="btn btn-lg btn-success"> Login Now</button>
