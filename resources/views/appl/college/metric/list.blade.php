5c58eb17f192f3402eb36541baa3e759b1f024ad
vagrant <vagrant@homestead>
2019-11-08 04:15:54 +0000

    some changes

--- resources/views/appl/college/metric/list.blade.php
+++ resources/views/appl/college/metric/list.blade.php
@@ -6,7 +6,6 @@
               <tr>
                 <th scope="col">#({{$objs->total()}})</th>
                 <th scope="col">Name </th>
-                <th scope="col">Student </th>
                 <th scope="col">Created at</th>
               </tr>
             </thead>
@@ -18,9 +17,6 @@
                   <a href=" {{ route($app->module.'.show',$obj->id) }} ">
                   {{ $obj->name }}
                   </a>
-                </td>
-                <td>
-                  {{ count($obj->users) }}
                 </td>
                 
                 <td>{{ ($obj->created_at) ? $obj->created_at->diffForHumans() : '' }}</td>
