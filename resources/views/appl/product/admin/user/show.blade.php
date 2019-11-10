63ed78ac5cf94a2cdc91b20c04442dddd5611dfd
vagrant <vagrant@homestead>
2019-11-10 06:27:11 +0000

    Soem cahnges

--- resources/views/appl/product/admin/user/show.blade.php
+++ resources/views/appl/product/admin/user/show.blade.php
@@ -275,7 +275,7 @@
         </h2></div>
       <div class="card-body">
         @if($user->products()->count())
-        <table class="table">
+        <table class="table table-bordered">
   <thead>
     <tr>
       <th scope="col">#</th>
