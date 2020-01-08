2324e4130e52d788f74d7c3dc1ebefc4d25a6431
vagrant <vagrant@homestead>
2020-01-08 15:34:15 +0000

    some changes

--- resources/views/layouts/head-noscript.blade.php
+++ resources/views/layouts/head-noscript.blade.php
@@ -18,13 +18,6 @@
 </head>
 <body>
   @yield('content-main')
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-43617911-7"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-43617911-7');
-  </script>
+ 
 </body>
 </html>
