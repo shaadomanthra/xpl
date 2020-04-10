682545107cf33b34540f5bb9c103ddcb085c7845
vagrant <vagrant@homestead>
2020-04-10 16:01:05 +0000

    some changes

--- public/js/capture.js
+++ public/js/capture.js
@@ -1,10 +1,9 @@
-
 (function() {
   // The width and height of the captured photo. We will set the
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
 
-  var width = 250;    // We will scale the photo width to this
+  var width = 320;    // We will scale the photo width to this
   var height = 0;     // This will be computed based on the input stream
 
   // |streaming| indicates whether or not we're currently streaming
@@ -25,6 +24,7 @@
     canvas = document.getElementById('canvas');
     photo = document.getElementById('photo');
     text = document.getElementById('text');
+    startbutton = document.getElementById('startbutton');
 
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(function(stream) {
@@ -54,8 +54,12 @@
       }
     }, false);
 
-   clearphoto();
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
     
+    clearphoto();
   }
 
   // Fill the photo with an indication that none has been
@@ -104,13 +108,38 @@
   }
 
   $time = $('#video').data('count');
-  setTimeout(function(){ takepicture(); }, $time);
+  // setTimeout(function(){ takepicture(); }, $time);
 
-  setInterval(takepicture(), 3000);
+  // setInterval(takepicture(), 3000);
+
+  setTimeout(function(){ takepicture(); }, 3000);
+
 
   // Set up our event listener to run the startup process
   // once loading is complete.
   window.addEventListener('load', startup, false);
 
+  function base64ToBlob(base64, mime) 
+{
+    mime = mime || '';
+    var sliceSize = 1024;
+    var byteChars = window.atob(base64);
+    var byteArrays = [];
+
+    for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
+        var slice = byteChars.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, {type: mime});
+}
 
 })();
