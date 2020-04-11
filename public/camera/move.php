795a4fe03a72fdb5ca003660e3d04b084de6f1e2
vagrant <vagrant@homestead>
2020-04-11 11:32:50 +0000

    some changes

--- public/camera/move.php
+++ public/camera/move.php
@@ -1,6 +1,7 @@
 <?php
 
-
-
-
-rename('main.css', getcwd().'/storage/cache/main.css');
+$output = shell_exec('python p.py'); 
+  
+// Display the list of all file 
+// and directory 
+echo "<pre>$output</pre>";
