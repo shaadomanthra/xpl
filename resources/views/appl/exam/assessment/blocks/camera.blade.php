2b8d02073e2661b9d769537fd94c3fcc0a884a13
vagrant <vagrant@homestead>
2020-04-11 10:23:21 +0000

    some changes

--- resources/views/appl/exam/assessment/blocks/camera.blade.php
+++ resources/views/appl/exam/assessment/blocks/camera.blade.php
@@ -1,13 +1,12 @@
 @if($cameratest)
 <div class="bg-light border rounded p-3 camera_fail mb-3" style="display:none">
-
-	<p>This test requires access to the webcamera. Kindly activate camera before proceeding. Incase of any query kindly reach out to the administration.</p>
+	<p><i class="fa fa-times-circle text-danger"></i> This test requires access to the webcamera. Kindly activate camera before proceeding. Incase of any query kindly reach out to the administration.</p>
 </div>
 
 <div class="bg-light border rounded p-3 camera_success mb-3" style="display:none">
 	<video id="video" class="mb-3 bg-light " width="300px" >Video stream not available.</video>
 	<canvas id="canvas" style='display: none'>
 	</canvas>
-	<b>Camera test is successful. Proceed to the test.</b>
+	<div><i class="fa fa-thumbs-up text-success"></i> Camera test is successful. Proceed to the test.</div>
 </div>
 @endif
