cf676aaa19298a33ba8a658c1c3530e3c3e5bee4
vagrant <vagrant@homestead>
2020-04-11 16:12:05 +0000

    some changes

---
+++ public/camera/face1.php
@@ -0,0 +1,4 @@
+<?php
+
+$cmd = 'python3 faceapp/facedetect2_completed.py face1.jpg';
+echo shell_exec($cmd);
---
+++ public/camera/face2.php
@@ -0,0 +1,4 @@
+<?php
+
+$cmd = 'python3 faceapp/facedetect2_completed.py f2.jpg';
+echo shell_exec($cmd);
