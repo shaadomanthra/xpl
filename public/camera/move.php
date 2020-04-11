7946f8d361447a0b7abe330f057d7bcf15895412
vagrant <vagrant@homestead>
2020-04-11 11:44:48 +0000

    some changes

--- public/camera/move.php
+++ public/camera/move.php
@@ -1,7 +1,7 @@
 <?php
 
-$output = shell_exec('python p.py'); 
+exec ("sudo python p.py 2>&1");
   
 // Display the list of all file 
 // and directory 
-echo "<pre>$output</pre>";
+echo "<pre>hello</pre>";
