d43347679c124d7d8161938d2d9176acf87d338c
vagrant <vagrant@homestead>
2020-04-11 10:01:35 +0000

    some changes

--- resources/views/appl/exam/assessment/show.blade.php
+++ resources/views/appl/exam/assessment/show.blade.php
@@ -104,8 +104,20 @@
 					<div class="bg-light border rounded p-3">You are not authorised to attempt this test.</div>
 					@endif
 			       @else
-			       <a href="#" data-toggle="modal" data-target="#myModal2">
-			       		<button class="btn btn-lg btn-outline-primary"> Access Code </button>
+			       	@if($cameratest)
+				   		<div class="bg-light border rounded p-3 camera_fail" style="display:none">
+				   			
+				   			<p>This test requires access to the webcamera. Kindly activate camera before proceeding. Incase of any query kindly reach out to the administration.</p>
+				   		</div>
+
+				   	<div class="bg-light border rounded p-3 camera_success" style="display:none">
+				   			<video id="video" class="mb-3 bg-light " width="300px" >Video stream not available.</video>
+						    <canvas id="canvas" style='display: none'>
+						  	</canvas>
+				   			<p>Camera test is successful</p></div>
+				   	@endif
+			       <a href="{{route('assessment.access',$exam->slug)}}">
+			       	<button class="btn btn-lg btn-outline-primary accesscode_btn"> Access Code </button>
 					</a>
 			       @endauth
 
