b76a967f0ce6536596f864b5a9e9bd30b7358940
vagrant <vagrant@homestead>
2020-05-09 09:21:56 +0000

    some changes

--- resources/views/appl/content/article/list.blade.php
+++ resources/views/appl/content/article/list.blade.php
@@ -6,7 +6,7 @@
 <div class="col-12 col-md-6 col-lg-4">
  <div class="card mb-3" >
    @if(Storage::disk('public')->exists($obj->image))
-   <div class="card-header" >
+   <div class="p-2" >
      <a href="{{ route('page',$obj->slug) }}" >
       
            <picture >
@@ -16,14 +16,14 @@
              {{ asset('/storage/articles/'.$obj->slug.'_1200.webp') }}  1100w" type="image/webp" sizes="(max-width: 320px) 280px,
             (max-width: 480px) 440px,
             (max-width: 720px) 800px
-            1200px" alt="{{  $obj->name }}" style="max-height:120px;width:auto;">
+            1200px" alt="{{  $obj->name }}" style="width:100%">
   <source srcset="{{ asset('/storage/articles/'.$obj->slug.'_300.jpg') }} 320w,
              {{ asset('/storage/articles/'.$obj->slug.'_600.jpg') }}  480w,
              {{ asset('/storage/articles/'.$obj->slug.'_900.jpg') }}  800w,
              {{ asset('/storage/articles/'.$obj->slug.'_1200.jpg') }}  1100w," type="image/jpeg" sizes="(max-width: 320px) 280px,
             (max-width: 480px) 440px,
             (max-width: 720px) 800px
-            1200px" alt="{{  $obj->name }}" style="max-height:120px;width:auto;"> 
+            1200px" alt="{{  $obj->name }}" style="width:100%"> 
   <img srcset="{{ asset('/storage/articles/'.$obj->slug.'_300.jpg') }} 320w,
              {{ asset('/storage/articles/'.$obj->slug.'_600.jpg') }}  480w,
              {{ asset('/storage/articles/'.$obj->slug.'_900.jpg') }}  800w,
@@ -32,17 +32,15 @@
             (max-width: 480px) 440px,
             (max-width: 720px) 800px
             1200px"
-      src="{{ asset('/storage/articles/'.$obj->slug.'_300.jpg') }} " class=" card-img-top" alt="{{  $obj->name }}" style="max-height:120px;width:auto;">
+      src="{{ asset('/storage/articles/'.$obj->slug.'_300.jpg') }} " class=" card-img-top" alt="{{  $obj->name }}" style="width:100%">
 </picture>
 
     </a>
     </div>
       @endif
   <div class="card-body">
-    <a href="{{ route('page',$obj->slug) }}" ><h2 class="card-title article">{{ $obj->name }}</h2></a>
-    <p class="card-text article">
-      {!! 
-      substr(strip_tags($obj->description),0,200) !!}@if(strlen(strip_tags($obj->description))>200) ... @endif</p>
+    <a href="{{ route('page',$obj->slug) }}" ><h4 class="card-title article">{{ $obj->name }}</h4></a>
+    
     <a href="{{ route('page',$obj->slug) }}" class="btn btn-success"><i class="fa fa-align-right"></i> read more</a>
   </div>
 </div>
