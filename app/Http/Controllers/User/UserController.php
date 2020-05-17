e8f52cff410b409beab62a46fac011d02bf01b5e
vagrant <vagrant@homestead>
2020-05-17 11:15:40 +0000

    some changes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -241,13 +241,18 @@ class UserController extends Controller
         $message = urlencode($message);
         $data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
 
+
+
         $ch = curl_init('http://api.textlocal.in/send/?');
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch); // This is the result from the API
         curl_close($ch);
-        
+
+        echo $data;
+        return 1;
+
     }
 
     public function saveregister(Request $request)
