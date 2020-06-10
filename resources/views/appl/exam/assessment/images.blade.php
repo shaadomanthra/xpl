9e900a946f7150f05b70d2493bbb0128ad0acdbb
vagrant <vagrant@homestead>
2020-06-10 12:38:48 +0000

    some changes

--- resources/views/appl/exam/assessment/images.blade.php
+++ resources/views/appl/exam/assessment/images.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <div class="card">
-	<div class="card-body">
+	<div class="card-body {{$c=0}}">
 @if(Storage::disk('public')->exists('tests/'.$user->username.'_'.$exam->id.'_1.jpg'))
 			<div class="row mb-4">
 				@for($i=1;$i<360;$i++)
 					@if(Storage::disk('public')->exists('tests/'.$user->username.'_'.$exam->id.'_'.$i.'.jpg'))
-					<div class='col-6 col-md-2'>
+					<div class='col-6 col-md-2 {{$c++}}'>
 						<img src="{{ asset('/storage/tests/'.$user->username.'_'.$exam->id.'_'.$i.'.jpg') }}" class="w-100 mb-2" />
 					</div>
 					@endif
@@ -16,5 +16,7 @@
 
 			</div>
 			@endif
+			<div class="mt-4">Captured: {{$c}} images</div>
 		</div>
 		</div>
+@endsection
