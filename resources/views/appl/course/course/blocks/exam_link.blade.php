b127a98148395d569dc64ffdb996aae7739ed155
vagrant <vagrant@homestead>
2020-06-12 16:23:46 +0000

    some changes

--- resources/views/appl/course/course/blocks/exam_link.blade.php
+++ resources/views/appl/course/course/blocks/exam_link.blade.php
@@ -24,8 +24,8 @@
        @endif
 @else
 
-@if(isset($eid))
-<a href="{{ route('assessment.analysis',$c->exam_id)}}">
+@if(isset($course->tests->$eid))
+<a href="{{ route('assessment.analysis',$course->tests->$eid)}}">
  <span class="badge badge-primary"> <i class="fa fa-bar-chart-o"></i> Test Analysis </span>
 </a>
 @endif
