468172be437b88e51d1b02a969b1201fbd189754
vagrant <vagrant@homestead>
2020-06-18 16:23:07 +0000

    some changes

--- resources/views/appl/pages/bfs/icons/superadmin.blade.php
+++ resources/views/appl/pages/bfs/icons/superadmin.blade.php
@@ -1,7 +1,38 @@
+
+<div class="row">
+	<div class="col-12 col-md-4">
+            <div class="   p-4 rounded mb-3 mr-2" style="background: #fffdf9;border:1px solid #f1e8d6">
+                <div class="">
+                    <div class=""><i class="fa fa-user"></i> Students</div>
+                    <div class="display-2">12</div>
+                </div>
+            </div>
+        
+	</div>
+	<div class="col-12 col-md-4">
+            <div class="   p-4 rounded mb-3 mr-2" style="background: #fffdf9;border:1px solid #f1e8d6">
+                <div class="">
+                    <div class=""><i class="fa fa-university"></i> Colleges</div>
+                    <div class="display-2">5</div>
+                </div>
+            </div>
+        
+	</div>
+	<div class="col-12 col-md-4 mb-2">
+            <div class="   p-4 rounded mb-4 mr-2" style="background: #fffdf9;border:1px solid #f1e8d6">
+                <div class="">
+                    <div class=""><i class="fa fa-desktop"></i> Trainers</div>
+                    <div class="display-2">2</div>
+                </div>
+            </div>
+        
+	</div>
+</div>
+
 <div class="row">
 	<div class="col-6 col-md-3">
 		<a href="{{ route('user.list') }}">
-            <div class="   p-4 rounded mb-3 mr-2" style="background: #fff4f8;border:1px solid #f1d6e0">
+            <div class="   p-4 rounded mb-3 mr-2" style="background: #fff9fb;border:1px solid #f1d6e0">
                 <div class="">
                     <img src="{{ asset('img/admin/class.png') }}" class="w-100 mb-3" >
                     <div class="text-center">Users</div>
@@ -11,7 +42,7 @@
 	</div>
 	<div class="col-6 col-md-3">
 		<a href="{{ route('exam.index') }}">
-            <div class=" p-4 rounded mb-3 mr-2" style="background: #fff4f8;border:1px solid #f1d6e0">
+            <div class=" p-4 rounded mb-3 mr-2" style="background: #fff9fb;border:1px solid #f1d6e0">
                 <div class="">
                     <img src="{{ asset('img/admin/online-test.png') }}" class="w-100 mb-3" >
                     <div class="text-center">Tests</div>
@@ -21,7 +52,7 @@
 	</div>
 	<div class="col-6 col-md-3">
 		<a href="{{ route('training.index') }}">
-            <div class=" p-4 rounded mb-3 mr-2" style="background: #fff4f8;border:1px solid #f1d6e0">
+            <div class=" p-4 rounded mb-3 mr-2" style="background: #fff9fb;border:1px solid #f1d6e0">
                 <div class="">
                     <img src="{{ asset('img/admin/professor.png') }}" class="w-100 mb-3" >
                     <div class="text-center">Trainings</div>
@@ -31,7 +62,7 @@
 	</div>
 	<div class="col-6 col-md-3">
 		<a href="{{ route('training.index') }}">
-            <div class="p-4 rounded mb-3 mr-2" style="background: #fff4f8;border:1px solid #f1d6e0">
+            <div class="p-4 rounded mb-3 mr-2" style="background: #fff9fb;border:1px solid #f1d6e0">
                 <div class="">
                     <img src="{{ asset('img/admin/assignment.png') }}" class="w-100 mb-3" >
                     <div class="text-center">Surveys</div>
