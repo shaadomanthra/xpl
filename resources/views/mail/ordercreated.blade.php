dfd24c2edbfb4b615d403cf47a4cd67ce68fd947
vagrant <vagrant@homestead>
2020-07-13 10:15:55 +0000

    some changes

--- resources/views/mail/ordercreated.blade.php
+++ resources/views/mail/ordercreated.blade.php
@@ -9,7 +9,7 @@ Transaction Amount : <b>{{$order['txn_amount']}}</b><br>
 Payment Status : <b>{{$order['payment_status']}}</b><br>
 @endcomponent
 
-In case of any query kindly <a href="{{route('contact-corporate')}}">contact</a> our admin team.<br><br>
+In case of any query kindly <a href="{{route('contact')}}">contact</a> our admin team.<br><br>
 Thanks,<br>
 Xplore
 @endcomponent
