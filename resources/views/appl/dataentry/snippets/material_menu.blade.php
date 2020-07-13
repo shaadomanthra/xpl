f3b9e2c7a56e5cd072ac30b507ea38834bdae2f6
vagrant <vagrant@homestead>
2020-07-13 08:36:24 +0000

    some changes

--- resources/views/appl/dataentry/snippets/material_menu.blade.php
+++ resources/views/appl/dataentry/snippets/material_menu.blade.php
@@ -4,7 +4,6 @@
 		<i class="fa fa-telegram"></i> Material Home 
 	</a>
 	<a href="{{ route('dataentry.index')}}" class="list-group-item list-group-item-action {{ request()->is('dataentry*') ? 'active' : ''}}">&nbsp;&nbsp;&nbsp;<i class="fa fa-inbox"></i> Data Entry</a>
-	<a href="{{ route('library.index')}}" class="list-group-item list-group-item-action {{ request()->is('library*') ? 'active' : ''}}">&nbsp;&nbsp;&nbsp;<i class="fa fas fa-university"></i> Library</a>
 	<a href="{{ route('course.index')}}" class="list-group-item list-group-item-action">&nbsp;&nbsp;&nbsp;<i class="fa fas fa-stack-exchange"></i> Courses</a>
 
 </div>
