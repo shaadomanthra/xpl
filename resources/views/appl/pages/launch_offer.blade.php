0ec09046b4f6107aa89c3f5448ebb4a2f4a4a44b
vagrant <vagrant@homestead>
2020-07-14 08:41:01 +0000

    some changes

--- resources/views/appl/pages/launch_offer.blade.php
+++ resources/views/appl/pages/launch_offer.blade.php
@@ -10,24 +10,26 @@
 <div class="row">
 	<div class="col-12 col-md-8">
 		<p>
- Launching the one stop online Learning and Assessment portal for all your knowledge and placement support services.</p>
+ Launching the one-stop online Learning and Assessment portal for all your knowledge and placement support services.</p>
 
+<p>An inaugural offer, now at  Rs 100 only  (Py now and get 2 months access to the portal)</p>
  <ul>
 	<li> Access to exclusive online video content on aptitude, logical, mental ability,
 programming & Interview Skills</li>
 <li>Timeline Mock & Practice Tests of Top MNCs with graphical reports of your performance analysis</li>
 <li>Verifiable e-certificate that counts as first level interview for some of our client organizations</li>
-<li>Get this opportunity to take part in India's first and largest online</li>
-<li>Career fair starts from July 27th during this pandemic with 40+ top Companies and around 2,000+ Jobs openings</li>
+<li>Utilize this service to get an advantage in India’s first, largest  and Free Virtual Career fair being organized by Xplore during this pandemic with 40+ top Companies and around 2,000+ Job openings </li>
 
 </ul>
+<p>For more details please call our toll free number: 1800-890-1324
+Or  9391002872( WhatsApp messages only)</p>
 <span class="badge badge-warning">Inaugral Offer</span><br>
 <div class="display-3 mb-3" >Rs. <span >100</span></div>
 
 <a href="https://xplore.co.in/productpage/aptitude-course" class="btn btn-primary btn-lg">Buy Now</a>
 	</div>
 	<div class="col-12 col-md-4">
-		<img src="{{ asset('img/launch_offer.jpg')}}" class="py-2 mb-3 w-100"  />
+		<img src="{{ asset('img/virtual-fair.jpg')}}" class="py-2 mb-3 w-100"  />
 	</div>
 </div>
 
