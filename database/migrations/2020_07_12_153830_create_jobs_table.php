4da4d3ca679c671c36e9888b9a5e27d20bc68c95
vagrant <vagrant@homestead>
2020-07-17 06:17:18 +0000

    some changes

--- database/migrations/2020_07_12_153830_create_jobs_table.php
+++ database/migrations/2020_07_12_153830_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobsTable23 extends Migration
+class CreateJobsTable232 extends Migration
 {
     /**
      * Run the migrations.
