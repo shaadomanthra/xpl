9198a6324d5f9f42c6d01d7335d7d2fb3b65e130
vagrant <vagrant@homestead>
2020-07-17 05:50:56 +0000

    some changes

--- database/migrations/2020_03_22_124611_update_questions-table_new.php
+++ database/migrations/2020_03_22_124611_update_questions-table_new.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateQuestionsTableNew extends Migration
+class UpdateQuestionsTableNew23 extends Migration
 {
     /**
      * Run the migrations.
