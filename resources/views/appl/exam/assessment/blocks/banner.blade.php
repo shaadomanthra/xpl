cd9de8fab3d81926b0dea5510fcaadf54c15a328
vagrant <vagrant@homestead>
2020-07-19 11:31:41 +0000

    some changes

--- resources/views/appl/exam/assessment/blocks/banner.blade.php
+++ resources/views/appl/exam/assessment/blocks/banner.blade.php
@@ -2,7 +2,7 @@
 <div class="my-3">
   <picture class="">
     <img 
-    src="{{ Storage::disk('s3')->exists('articles/'.$exam->slug.'_'.strtolower(str_replace(' ', '', $user->hometown).'.jpg')) }} " class="d-print-none w-100" alt="{{  $exam->name }}" >
+    src="{{ Storage::disk('s3')->url('articles/'.$exam->slug.'_'.strtolower(str_replace(' ', '', $user->hometown).'.jpg')) }} " class="d-print-none w-100" alt="{{  $exam->name }}" >
   </picture>
 </div>
 @elseif(Storage::disk('s3')->exists('articles/'.$exam->slug.'_banner.jpg'))
