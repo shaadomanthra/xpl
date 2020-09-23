1aad4d5e79dde083dbd51e35f302648e9b479152
vagrant <vagrant@homestead>
2020-09-23 13:28:52 +0000

    some changes

--- public/js/test.js
+++ public/js/test.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
                   $('.testpage').show();
               }
             }else{
-              $('.testpage').show();
+    
             }
             
         });
