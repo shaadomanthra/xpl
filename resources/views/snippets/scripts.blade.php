28f082d39e18f9120d57445da8ced49e226d03fc
vagrant <vagrant@homestead>
2020-09-26 03:21:47 +0000

    some changes

--- resources/views/snippets/scripts.blade.php
+++ resources/views/snippets/scripts.blade.php
@@ -1820,7 +1820,7 @@ $(function(){
 @if(isset($timer2))
 
 <script src="{{ asset('js/screenfull.min.js')}}"></script>
-<script src="{{ asset('js/test.js')}}?new=1"></script>
+<script src="{{ asset('js/test.js')}}?new=2"></script>
 <script type="text/javascript">
 $(function(){
 
