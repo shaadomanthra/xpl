2443e8360cb4d1ee9eb40a0aaef3fc01de8d4b7f
vagrant <vagrant@homestead>
2020-10-10 11:15:43 +0000

    some changes

--- app/Http/Controllers/Exam/AssessmentController.php
+++ app/Http/Controllers/Exam/AssessmentController.php
@@ -2641,6 +2641,7 @@ class AssessmentController extends Controller
                     if($completed_list[$content['username']]==1)
                         $content['completed'] = 1;
                 }else{
+                    if(isset($content['username']))
                     if($completed_list[$content['username']]==1)
                         $content['completed'] = 1;
                 }
