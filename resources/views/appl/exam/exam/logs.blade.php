3cb8391fb926ddf0cb32da594d3c74f490a7756a
vagrant <vagrant@homestead>
2020-11-08 07:52:34 +0000

    some changes

--- resources/views/appl/exam/exam/logs.blade.php
+++ resources/views/appl/exam/exam/logs.blade.php
@@ -47,7 +47,7 @@
         <div>OS details: <b><span class="log_os text-muted">{{$content['os_details']}}</span></b></div>
         <div>Browser details: <b><span class="log_browser text-muted">{{$content['browser_details']}}</span></b></div>
         <div>IP Address: <b><span class="log_ip text-muted">{{$content['ip_details']}}</span></b></div>
-        <div>Window Swaps: <b><span class="log_swaps text-danger">{{$content['window_swap']}}</span></b></div>
+        <div>Window Swaps: <b><span class="log_swaps text-danger">{{$content['window_change']}}</span></b></div>
         <div>Date: <b><span class="log_swaps text-primary">{{date("jS F, Y", $content['last_updated'])}}</span></b></div>
         <hr>
       </div>
