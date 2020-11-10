8c4779592cfb9ce498291485e08d77e6ae10a127
vagrant <vagrant@homestead>
2020-11-10 13:56:16 +0000

    some changes

--- app/Http/Middleware/nocache.php
+++ app/Http/Middleware/nocache.php
@@ -17,7 +17,7 @@ class nocache
     {
 
         $response = $next($request);
-        if(!$request->get('export') && !$request->get('student'))
+        if(!$request->get('export') && !$request->get('student') && !$request->get('downloadexport'))
         {
             $response->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
             $response->header('Cache-Control', 'no-cache, must-revalidate, no-store, max-age=0, private');
