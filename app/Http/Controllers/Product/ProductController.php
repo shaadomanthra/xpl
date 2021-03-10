ff5fcde53d0ad9eb1a2213b2b17b1910767ee8f4
Krishna Teja <founder@packetprep.com>
2021-03-10 16:13:32 +0530

    some changes

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -245,7 +245,7 @@ class ProductController extends Controller
           //$user->exams()->withCount('users')->orderBy('id','desc');
 
 
-          $count = Tests_Overall::whereIn('test_id',$usertests->pluck('id')->toArray())->count();
+          $count = 0;//Tests_Overall::whereIn('test_id',$usertests->pluck('id')->toArray())->count();
           //$count = count($alltests);
 
          
