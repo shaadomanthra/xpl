b2167b4c5c4f442c8ada53aa385cd40c6a2fd95c
Krishna Teja <founder@packetprep.com>
2021-05-12 15:48:12 +0530

    some changes

--- app/Jobs/SendEmail.php
+++ app/Jobs/SendEmail.php
@@ -47,11 +47,11 @@ class SendEmail implements ShouldQueue
     public function handle()
 
     {
-         if($this->details['maillog']){
+         //if($this->details['maillog']){
             $m = MailLog::where('id',$this->details['maillog'])->first();
             $m->status = 1;
             $m->save();
-       }
+       //}
         Mail::to($this->details['email'])->send(new EmailForQueuing($this->details,$this->subject,$this->content));
     }
 }
