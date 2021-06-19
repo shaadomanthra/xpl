51e9c165b38c1ad49375235fac9df011d5f66e21
Krishna Teja <founder@packetprep.com>
2021-06-19 09:07:03 +0530

    some changes

--- resources/views/appl/exam/exam/xp_css.blade.php
+++ resources/views/appl/exam/exam/xp_css.blade.php
@@ -2,10 +2,10 @@
 <style>
   .lblue{  background: #f8f8f8;}
 .dblue{background:#fff5f5}
-.ddblue{ background: #fac9c9; }
+.ddblue{ background:#fe8b8b; }
 .heading_two{ color:#2a2d31; }
 .heading_one{ color:#b7282d; }
-.cardbox{border:1px solid #fff;box-shadow:2px 3px #fcdfdf;}
+.cardbox{border:1px solid #fff;box-shadow:2px 3px #ffe7e7;}
 .lblue{background: #fcfafa}
 .pill{padding:0px 7px;border-radius: 3px;font-size: 12px;color:#949494;}
 
