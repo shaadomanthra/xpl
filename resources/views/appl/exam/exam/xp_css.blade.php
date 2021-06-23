c502fdb5a0d1a55f7c55490e823b46f44265ef29
Krishna Teja <founder@packetprep.com>
2021-06-23 12:49:01 +0530

    some changes

--- resources/views/appl/exam/exam/xp_css.blade.php
+++ resources/views/appl/exam/exam/xp_css.blade.php
@@ -1,13 +1,12 @@
 @if($_SERVER['HTTP_HOST'] == 'xp.test' || $_SERVER['HTTP_HOST'] == 'xplore.co.in')
 <style>
-  .lblue{  background: #f8f8f8;}
+.lblue{  background: #f8f8f8;}
 .dblue{background:#fff5f5}
 .ddblue{ background:#fe8b8b; }
 .heading_two{ color:#2a2d31; }
 .heading_one{ color:#b7282d; }
-.cardbox{border:1px solid #fff;box-shadow:2px 3px #ffe7e7;}
+.cardbox{border:1px solid #f3d3d3;box-shadow:2px 3px #ffe7e7;  }
 .lblue{background: #fcfafa}
 .pill{padding:0px 7px;border-radius: 3px;font-size: 12px;color:#949494;}
-
 </style>
 @endif
