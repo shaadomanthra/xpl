b7dd097492c61cab919ac9fe3c4267ca9549a697
Krishna Teja <founder@packetprep.com>
2021-07-27 11:42:18 +0530

    some changes

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies =['10.139.0.2','159.65.232.195','10.139.0.10'];
+    protected $proxies =['10.139.0.2','159.65.232.195','10.139.0.10','10.139.0.13','10.139.0.12'];
 
     /**
      * The current proxy header mappings.
