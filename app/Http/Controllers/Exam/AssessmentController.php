570dddb5b184f2a0bd3a99dd949cd32cd8438e4d
shaadomanthra <shaadomanthra@yahoo.com>
2022-04-24 17:29:34 +0530

    some changes

--- app/Http/Controllers/Exam/AssessmentController.php
+++ app/Http/Controllers/Exam/AssessmentController.php
@@ -6340,7 +6340,13 @@ class AssessmentController extends Controller
         $user_id = $student->id;
         $test_id = $exam->id;
 
-        $tests_overall = Tests_Overall::where('test_id',$exam->id)->where('user_id',$student->id)->first();
+        $tests_overall = Cache::remember('attempt_'.$user_id.'_'.$test_id, 60, function() use ($exam,$student){
+            return Tests_Overall::where('test_id',$exam->id)->where('user_id',$student->id)->first();
+        });
+
+        if(!$tests_overall){
+             $tests_overall  = Tests_Overall::where('test_id',$exam->id)->where('user_id',$student->id)->first();
+        }
 
         $data['attempt'] = $tests_overall;
         $data['exam'] = $exam;
