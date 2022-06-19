0ef5137b035f622e56663b9a79a14679936dfc10
shaadomanthra <shaadomanthra@yahoo.com>
2022-06-19 14:03:42 +0530

    some changes

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -569,6 +569,9 @@ class ProductController extends Controller
     {
         $user = \auth::user();
 
+        if(request()->get('role'))
+          dd($user->role);
+        
         if(!$user)
           abort(403,'Login to view th page!');
 
@@ -578,8 +581,7 @@ class ProductController extends Controller
         $product= Product::where('slug',$id)->with('orders')->first();
         $orderids = $product->orders->pluck('id')->toArray();
         
-        if(request()->get('role'))
-          dd($user->role);
+
         $orders = Order::whereIn('id',$orderids)->orderBy('id','desc')->with('user')->paginate(20);
 
         $status=array("incomplete"=>0,"complete"=>0,"total"=>0);
