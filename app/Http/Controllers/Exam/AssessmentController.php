08027169bfe66a08fc630ba6ae466000669a4812
shaadomanthra <shaadomanthra@yahoo.com>
2022-08-04 21:56:13 +0530

    some changes

--- app/Http/Controllers/Exam/AssessmentController.php
+++ app/Http/Controllers/Exam/AssessmentController.php
@@ -2132,7 +2132,9 @@ class AssessmentController extends Controller
                     $review = true;
             }else{
 
+                if(isset($ques[$t->question_id]->mark)))
                 $cx = $cx+intval($ques[$t->question_id]->mark);
+                
                 
             }
             //$ques = Question::where('id',$q->id)->first();
