4dc7218bf3c0da20fafed6e76d68f8a1d629afe9
shaadomanthra <shaadomanthra@yahoo.com>
2023-02-23 13:57:01 +0530

    some changes

--- app/Http/Controllers/Dataentry/QuestionController.php
+++ app/Http/Controllers/Dataentry/QuestionController.php
@@ -1582,7 +1582,6 @@ class QuestionController extends Controller
         $question->tags = $question->tags->pluck('id')->toArray();         
 
      
-     
 
         if($question)
             return view('appl.dataentry.question.createedit')
