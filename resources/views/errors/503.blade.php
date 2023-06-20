f27f07e445f1bf1ccb1f17749cfab53d66f80101
shaadomanthra <shaadomanthra@yahoo.com>
2023-06-20 06:52:12 +0530

    some changes

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,10 +3,16 @@
 @section('content')
 <div class="mt-5">
 
-	<div class="text-center">
+	<div class="text-center d-none">
 		<h1 style="font-size:40px">Site is Down</h1>
 		
 		<p>Website under maintenance. Kindly check back after few minutes.</p>
+		
+	</div>
+	<div class="text-center">
+		<h1 style="font-size:40px">Forge not renewed.</h1>
+		
+		<p>Webservice is temporarily suspended. Kindly check back after few minutes.</p>
 		
 	</div>
 </div>
