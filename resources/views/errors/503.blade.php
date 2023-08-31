c5291cd49292d6b31c077aaf8a9cf4ff967f9bda
shaadomanthra <shaadomanthra@yahoo.com>
2023-08-31 06:59:20 +0530

    some changes

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -10,7 +10,7 @@
 		
 	</div>
 	<div class="text-center" >
-		<h1 style="font-size:40px">Forge not renewed.</h1>
+		<h1 style="font-size:40px">AWS services on hold.</h1>
 		
 		<p>Webservice is temporarily suspended. Kindly check back after few minutes.</p>
 		
